ad016665273df4a7a188bb7eeb8169f1f1819d8d
aswinbkk <aswinbaburajkk@gmail.com>
2023-04-29 09:31:43 +0530

    Fire Bird V code updated.

--- firebird_v_code/Fire Bird V/Fire Bird V.c
+++ firebird_v_code/Fire Bird V/Fire Bird V.c
@@ -85,13 +85,29 @@ void adc_pin_config (void)
 	PORTK = 0x00; //set PORTK pins floating
 }
 
+//Configure PORTB 6 pin for servo motor 2 operation
+void servo2_pin_config (void)
+{
+	DDRB  = DDRB | 0x40;  //making PORTB 6 pin output
+	PORTB = PORTB | 0x40; //setting PORTB 6 pin to logic 1
+}
+
+//Configure PORTB 7 pin for servo motor 3 operation
+void servo3_pin_config (void)
+{
+	DDRB  = DDRB | 0x80;  //making PORTB 7 pin output
+	PORTB = PORTB | 0x80; //setting PORTB 7 pin to logic 1
+}
+
 //Function to initialize ports
 void port_init()
 {
 	pin_config();
 	left_encoder_pin_config(); //left encoder pin config
 	right_encoder_pin_config(); //right encoder pin config
-	adc_pin_config();	
+	servo2_pin_config(); //Configure PORTB 6 pin for servo motor 2 operation
+	servo3_pin_config(); //Configure PORTB 7 pin for servo motor 3 operation
+	adc_pin_config();
 }
 
 //Function to Initialize ADC
@@ -544,14 +560,126 @@ uint8_t USART_Receive(void) {
 	return UDR0;
 }
 
+//TIMER1 initialization in 10 bit fast PWM mode  
+//prescale:256
+// WGM: 7) PWM 10bit fast, TOP=0x03FF
+// actual value: 42.187Hz 
+void timer1_init(void)
+{
+ TCCR1B = 0x00; //stop
+ TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
+ TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
+ OCR1BH = 0x03;	//Output compare Register high value for servo 2
+ OCR1BL = 0xFF;	//Output Compare Register low Value For servo 2
+ OCR1CH = 0x03;	//Output compare Register high value for servo 3
+ OCR1CL = 0xFF;	//Output Compare Register low Value For servo 3
+ ICR1H  = 0x03;	
+ ICR1L  = 0xFF;
+ TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
+ 					For Overriding normal port functionality to OCRnA outputs.
+				  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
+ TCCR1C = 0x00;
+ TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+}
+
+// Timer 5 initialized in PWM mode for velocity control
+// Prescale:64
+// PWM 8bit fast, TOP=0x00FF
+// Timer Frequency:674.988Hz
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+
+// Function for robot velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
 void init_devices (void)
 {
 	cli(); //Clears the global interrupts
 	port_init();
+	timer1_init();
+	timer5_init();
 	adc_init();
 	left_position_encoder_interrupt_init();
 	right_position_encoder_interrupt_init();
 	sei(); //Enables the global interrupts
+}
+
+//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+void servo_2(unsigned char degrees)
+{
+	float PositionTiltServo = 0;
+	PositionTiltServo = ((float)degrees / 1.86) + 35.0;
+	OCR1BH = 0x00;
+	OCR1BL = (unsigned char) PositionTiltServo;
+}
+
+//Function to rotate Servo 3 by a specified angle in the multiples of 1.86 degrees
+void servo_3(unsigned char degrees)
+{
+	float PositionServo = 0;
+	PositionServo = ((float)degrees / 1.86) + 35.0;
+	OCR1CH = 0x00;
+	OCR1CL = (unsigned char) PositionServo;
+}
+
+//servo_free functions unlocks the servo motors from the any angle
+//and make them free by giving 100% duty cycle at the PWM. This function can be used to
+//reduce the power consumption of the motor if it is holding load against the gravity.
+
+void servo_2_free (void) //makes servo 2 free rotating
+{
+	OCR1BH = 0x03;
+	OCR1BL = 0xFF; //Servo 2 off
+}
+
+void servo_3_free (void) //makes servo 3 free rotating
+{
+	OCR1CH = 0x03;
+	OCR1CL = 0xFF; //Servo 3 off
+}
+
+void lift (void) // grip the object
+{
+	servo_2 (1);
+	_delay_ms(1000);
+}
+
+void down (void) // relase the object
+{
+	servo_2 (90);
+	_delay_ms(1000);
+
+}
+
+void ungrip (void)
+{
+	servo_3 (40);
+	_delay_ms(1000);
+}
+
+void grip (void)
+{
+	servo_3 (80);
+	_delay_ms(1000);
 }
 
 void check_pin()
@@ -646,9 +774,15 @@ void move_robot()
 int main(void)
 {
 	USART_Init(); // initialize UART communication
+	unsigned char i = 0;
 	init_devices();
 	lcd_set_4bit();
 	lcd_init();
+	
+	lift();
+	_delay_ms(2000);
+	grip();
+	_delay_ms(2000);
 	
 	sharp = ADC_Conversion(11);						//Stores the Analog value of front sharp connected to ADC channel 11 into variable "sharp"
 	value = Sharp_GP2D12_estimation(sharp);				//Stores Distance calsulated in a variable "value".
@@ -687,12 +821,30 @@ int main(void)
 				move_robot ();
 			}
 			
+			down();
+			_delay_ms(2000);
+			ungrip();
+			_delay_ms(2000);
+			grip();
+			_delay_ms(2000);
+			lift();
+			_delay_ms(2000);
+			
 			check_pin();
 			
 			while ((start_column != column2) || (start_row != row2))
 			{
 				move_robot ();
 			}
+			
+			down();
+			_delay_ms(2000);
+			ungrip();
+			_delay_ms(2000);
+			lift();
+			_delay_ms(2000);
+			grip();
+			_delay_ms(2000);
 		}
 	}
 }
